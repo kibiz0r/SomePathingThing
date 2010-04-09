21e9be7ab828688ae5e91041a4963c33497b055b
Michael Harrington <kibiz0r@gmail.com>
2010-04-09 00:48:32 -0400

    Looks kind of cool now.

--- Main.cs
+++ Main.cs
@@ -25,8 +25,7 @@ namespace SomePathingThing
                 var width = bitmap.Width;
                 var height = bitmap.Height;
                 var inGroup = new bool[height, width];
-                var groups = new List<List<Point2>>();
-                var extremityGroups = new List<List<Point2>>();
+                var groups = new List<Rectangle>();
                 for (var startY = 0; startY < height; startY++)
                 {
                     for (var startX = 0; startX < width; startX++)
@@ -35,71 +34,54 @@ namespace SomePathingThing
                         {
                             continue;
                         }
+                        inGroup[startY, startX] = true;
                         Color desired = bitmap.GetPixel(startX, startY);
-                        var group = new List<Point2>();
-                        var extremities = new List<Point2>();
-                        var stack = new Stack<Point2>();
-                        stack.Push(new Point2(startX, startY));
-                        while (stack.Count > 0)
+                        var endX = startX;
+                        while (true)
                         {
-                            var pixel = stack.Pop();
-                            var x = pixel.X;
-                            var y = pixel.Y;
-                            if (inGroup[y, x] || bitmap.GetPixel(x, y) != desired)
+                            if (!(endX + 1 < width))
                             {
-                                continue;
+                                break;
                             }
-                            group.Add(new Point2(x, y));
-                            inGroup[y, x] = true;
-                            bool pure = true;
-                            if (y - 1 >= 0)
+                            if (bitmap.GetPixel(endX + 1, startY) != desired)
                             {
-                                stack.Push(new Point2(x, y - 1));
-                                if (bitmap.GetPixel(x, y - 1) != desired)
+                                break;
+                            }
+                            if (inGroup[startY, endX + 1])
+                            {
+                                break;
+                            }
+                            inGroup[startY, endX + 1] = true;
+                            endX++;
+                        }
+                        var endY = startY;
+                        for ( ; endY + 1 < height; endY++)
+                        {
+                            for (var x = startX; x <= endX; x++)
+                            {
+                                if (bitmap.GetPixel(x, endY + 1) != desired)
                                 {
-                                    pure = false;
+                                    goto foundY;
+                                }
+                                if (inGroup[endY + 1, x])
+                                {
+                                    goto foundY;
                                 }
                             }
-                            if (x - 1 >= 0)
+                            // Have to do this after the initial loop since we only want lines where the whole thing matches.
+                            for (var x = startX; x <= endX; x++)
                             {
-                                stack.Push(new Point2(x - 1, y));
-                                if (bitmap.GetPixel(x - 1, y) != desired)
-                                {
-                                    pure = false;
-                                }
-                            }
-                            if (y + 1 < height)
-                            {
-                                stack.Push(new Point2(x, y + 1));
-                                if (bitmap.GetPixel(x, y + 1) != desired)
-                                {
-                                    pure = false;
-                                }
-                            }
-                            if (x + 1 < width)
-                            {
-                                stack.Push(new Point2(x + 1, y));
-                                if (bitmap.GetPixel(x + 1, y) != desired)
-                                {
-                                    pure = false;
-                                }
-                            }
-                            if (!pure)
-                            {
-                                extremities.Add(new Point2(x, y));
+                                inGroup[endY + 1, x] = true;
                             }
                         }
-                        groups.Add(group);
-                        extremityGroups.Add(extremities);
+                    foundY:
+                        var rectangle = new Rectangle(startX, startY, endX, endY);
+                        groups.Add(rectangle);
                     }
                 }
                 Console.WriteLine("{0} groups", groups.Count);
-                Console.WriteLine("{0} extremety groups", extremityGroups.Count);
-                Color[] colors = new Color[]
-                {
-                    new Color(1, 0, 0),
-                    new Color(0, 1, 0)
-                };
+                var colors = (from g in groups
+                    select new Color((float)g.X2 / width, (float)g.Y2 / height, ((float)g.X1 + g.Y1) / (width + height))).ToArray();
                 while (true)
                 {
                     KeyboardState keyboardState = new KeyboardState();
@@ -109,13 +91,10 @@ namespace SomePathingThing
                         break;
                     }
                     bitmap.Draw(0, 0, DrawFlags.None);
-                    for (var i = 0; i < extremityGroups.Count; i++)
+                    for (var i = 0; i < groups.Count; i++)
                     {
-                        var extremities = extremityGroups[i];
-                        for (var j = 0; j + 1 < extremities.Count; j++)
-                        {
-                            Primitives.DrawLine(extremities[j].X, extremities[j].Y, extremities[j + 1].X, extremities[j + 1].Y, colors[i], 0);
-                        }
+                        var group = groups[i];
+                        Primitives.DrawRectangle(group.X1, group.Y1, group.X2, group.Y2, colors[i], 0);
                     }
                     Display.Flip();
                 }
---
+++ Rectangle.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace SomePathingThing
+{
+    public struct Rectangle
+    {
+        public Rectangle(int x1, int y1, int x2, int y2)
+        {
+            this.X1 = x1;
+            this.Y1 = y1;
+            this.X2 = x2;
+            this.Y2 = y2;
+        }
+
+        public readonly int X1;
+        public readonly int Y1;
+        public readonly int X2;
+        public readonly int Y2;
+    }
+}
